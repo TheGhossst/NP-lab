f0eaacfdc6026ba8f320c221466e87abf462da8a
TheGhossst <nandukrishnapkm97@gmail.com>
2024-05-27 16:32:25 +0530

    smtp

---
+++ smtp/client.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define bufferSize 1024
+
+int main(){
+	printf("SMTP Client\n.......\n");
+	int clientSocket;
+	struct sockaddr_in server_addr;
+	
+	int len = sizeof(server_addr);
+	char buffer[bufferSize] = {0};
+	
+	if((clientSocket = socket(AF_INET,SOCK_STREAM, 0)) == 0 ){
+		perror("Socket Error");
+		exit(EXIT_FAILURE);
+	}
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if(connect(clientSocket,(struct sockaddr *)&server_addr,sizeof(server_addr)) < 0) {
+		perror("Connection Error");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("The message should be in the format To:From:Subject:Body \n");
+	printf("\nEnter the message :\n");
+
+	fgets(buffer,sizeof(buffer),stdin);
+	send(clientSocket,buffer,strlen(buffer),0);
+
+	printf("\nSuccesfully send message to SMTP server\n");
+	close(clientSocket);
+}
---
+++ smtp/server.c
@@ -0,0 +1,69 @@
+//B21CS1235
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define bufferSize 1024
+
+int main(){
+        printf("\nSMTP SERVER\n");
+    int serverSocket,clientSocket;
+    struct sockaddr_in server_addr,client_addr;
+    int opt = 1;
+    int len = sizeof(server_addr);
+    char buffer[bufferSize] = {0};
+    
+    printf("\nSMTP Server starting ....");
+    
+    if((serverSocket = socket(AF_INET,SOCK_STREAM, 0)) == 0 ){
+        perror("Socket Error");
+        exit(EXIT_FAILURE);
+    }
+    if(setsockopt(serverSocket,SOL_SOCKET,SO_REUSEADDR | SO_REUSEPORT,&opt,sizeof(opt))){
+        perror("Options Error");
+        exit(EXIT_FAILURE);
+    }
+    server_addr.sin_family = AF_INET; 
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+    
+    if(bind(serverSocket,(struct sockaddr *)&server_addr,sizeof(server_addr) ) < 0){
+        perror("Bind Error");
+        exit(EXIT_FAILURE);
+    }
+    
+    if(listen(serverSocket,3) < 0){
+        perror("Listen Error");
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("\nConnection Established!\n");
+    int val;
+    char readArray[bufferSize];
+    while(1){
+        clientSocket = accept(serverSocket,(struct sockaddr *)&server_addr,(socklen_t *)&len);
+        char to_email[1024],from_email[1024],subject[1024],message[1024];
+
+              recv(clientSocket, to_email , sizeof(to_email), 0);
+              recv(clientSocket, from_email , sizeof(from_email), 0);
+              recv(clientSocket, Subject , sizeof(Subject), 0);
+              recv(clientSocket, message , sizeof(message), 0);
+
+              char *token = strtok(to_email,"@"); //nandu
+              token = strtok(NULL,"@"); //123 -> to domain
+              
+               char *token1 = strtok(from_email,"@"); //nandu
+              token1 = strtok(NULL,"@"); //123*/ -> from domain
+
+                printf(to,from,sub,mesage,token,token1);
+
+        printf("Data recieved from client\n\n");
+        printf("%s\n",buffer);
+        tokenize(buffer);    
+    }
+    close(clientSocket);
+    close(serverSocket);
+}
